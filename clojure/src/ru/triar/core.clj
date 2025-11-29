6133130612a0aa6e9db94ba042f322a944a4e35e
rururu <zspovenetsky@gmail.com>
2025-11-29 23:30:46 +0300

    Version 0.1.0 done

--- clojure/src/ru/triar/core.clj
+++ clojure/src/ru/triar/core.clj
@@ -5,16 +5,18 @@
     [ru.triar.logic :as lg]))
 
 (defn triar []
-  (let [f (gi/jframe-grid "Triangle Area" 7 1 4 16 200 300)
+  (let [r (volatile! nil)
+        f (gi/jframe-grid "Triangle Area" 7 1 4 16 200 300)
         _ (gi/add-label f "Triangle sides:")
-        a (gi/add-text-field f "a")
-        b (gi/add-text-field f "b")
-        c (gi/add-text-field f "c")
+        a (gi/add-text-field f "1")
+        b (gi/add-text-field f "2")
+        c (gi/add-text-field f "3")
         _ (gi/add-label f "Triangle area:")
         _ (gi/add-button f "calculate area"
-            (fn[_](lg/calc-area 
-                    (.getText a)
-                    (.getText b)
-                    (.getText c))))
-        r (gi/add-text-field f "area")]
+            (fn[_] (.setText @r (str
+              (lg/calc-area
+                (read-string (.getText a))
+                (read-string (.getText b))
+                (read-string (.getText c)))))))]
+      (vreset! r (gi/add-text-field f "area"))
       (.setVisible f true)))
--- clojure/src/ru/triar/logic.clj
+++ clojure/src/ru/triar/logic.clj
@@ -1,5 +1,11 @@
 ;; logic.clj
 (ns ru.triar.logic)
-(defn input-data [a b c]
-  (println "input-data" a b c))
+(defn perimetr [a b c]
+  (+ a b c))
+  
+(defn calc-area [a b c]
+  (let [p (perimetr a b c)
+        hp (/ p 2)
+        g (* hp (- hp a)(- hp b)(- hp c))]
+    (Math/sqrt g)))
 
